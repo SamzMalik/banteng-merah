624e3b4174f061d417cb443d1055686d71645ea8
SamzMalik <hisyamabdulmalik04@gmail.com>
2023-12-05 18:04:06 +0700

    webp

--- src/data/comments.ts
+++ src/data/comments.ts
@@ -8,25 +8,25 @@ type commentsType = {
 
 export const testimonialComments: commentsType[] = [
     {
-        companyImg: 'https://github.com/SamzMalik/samz-img-collections/blob/main/pwdk/gojek.1024x1024.png?raw=true',
+        companyImg: 'https://raw.githubusercontent.com/SamzMalik/samz-img-collections/main/pwdk/gojek.1024x1024.webp',
         userName: 'Michael Angelo Moran',
         userRole: 'Application Manager Gojek',
         userComment: 'We chose to trust Icehouse for developing our first mobile applicatons and it was awesome!'
     },
     {
-        companyImg: 'https://github.com/SamzMalik/samz-img-collections/blob/main/pwdk/mybca.png?raw=true',
+        companyImg: 'https://raw.githubusercontent.com/SamzMalik/samz-img-collections/main/pwdk/mybca.webp',
         userName: 'Hudiyono Sarim',
         userRole: 'CEO at MyBCA',
         userComment: 'Exceptional mobile app development! Your agency seamlessly blends innovation and usability, delivering apps that not only meet but exceed expectations',
     },
     {
-        companyImg: 'https://github.com/SamzMalik/samz-img-collections/blob/main/pwdk/bluebird.png?raw=true',
+        companyImg: 'https://raw.githubusercontent.com/SamzMalik/samz-img-collections/main/pwdk/bluebird.webp',
         userName: 'Agung Laksono',
         userRole: 'CTO at Bluebird',
         userComment: 'Kudos to your team for crafting user-friendly and visually stunning mobile apps. Your agency commitment to excellence shines through in every project!',
     },
     {
-        companyImg: 'https://github.com/SamzMalik/samz-img-collections/blob/main/pwdk/prodia.png?raw=true',
+        companyImg: 'https://raw.githubusercontent.com/SamzMalik/samz-img-collections/main/pwdk/prodia.webp',
         userName: 'Tandy Rijaya',
         userRole: 'Senior Project Manager at Prodia',
         userComment: 'Your mobile app agency has been a game-changer for our business. The innovative solutions you provide have elevated our digital presence, and we appreciate the dedication of your skilled team.',
