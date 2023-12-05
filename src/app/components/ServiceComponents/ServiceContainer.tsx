6ac61568b2ee188b96b444d89d6448bf8d8805ae
SamzMalik <hisyamabdulmalik04@gmail.com>
2023-12-05 08:16:42 +0700

    text counter

--- src/app/components/ServiceComponents/ServiceContainer.tsx
+++ src/app/components/ServiceComponents/ServiceContainer.tsx
@@ -8,6 +8,7 @@ const ServiceContainer = () => {
                 <div className='max-w-xl text-primary-white  bg-blue-600 rounded-xl p-4 lg:h-[612px]'>
                     <Image alt='something' width={512} height={512} className='w-[422px]' src='/Software-Development.png' />
                     <div className='p-2 lg:p-4'>
+                        <h1 className='text-lg lg:text-3xl font-bold'>Software Development</h1>
                         <p className='text-sm lg:text-md lg:text-lg'>Our agile product development teams are structured specifically around the needs of your project. This allows us to quickly adapt to: shifting product needs, evolving markets, and any internal project factors as needed. In return, you receive test builds along with a test app presented every 2 weeks.</p>
                     </div>
                 </div>
